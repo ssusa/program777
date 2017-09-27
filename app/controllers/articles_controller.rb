dad6d04d7c7d916e5718d50c6283cbf4daca5c5f
lupin <shintaro7susa3@gmail.com>
2017-09-28 01:59:24 +0900

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,16 @@
 class ArticlesController < ApplicationController
   def show
     if params[:yyyymm].present? && params[:yyyymmdd].present?
-
       #これをどうにかしたい
       if params[:yyyymmdd] == "20170926"
         @page_title = "1.事前準備 railsタスク管理"
       elsif params[:yyyymmdd] == "20170927"
         @page_title = "2.作成するアプリの仕様 railsタスク管理"
       end
-
       render "articles/" + params[:yyyymm] + "/" + params[:yyyymmdd]
     else
+      #デフォルト
+      @page_title = "1.事前準備 railsタスク管理"
       render "articles/201709/20170926"
     end
   end
