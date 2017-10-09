af143205bbf4269986264a3607c6e7095080b1a0
lupin <shintaro7susa3@gmail.com>
2017-10-10 03:41:07 +0900

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,36 @@
 #coding: utf-8
 class ArticlesController < ApplicationController
+
+  TK_TITLE1 = "Ruby on Railsでタスク管理を作ろう！"
+  TK_YMD = [
+    "20170926",
+    "20170927",
+    "20170930",
+    "20171004",
+    "20171005",
+    "20171009",
+    "99999999"
+    ]
+  TK_TITLE2 = [
+    "事前準備",
+    "作成するアプリの仕様",
+    "railsプロジェクト作成",
+    "データベース作成",
+    "テストデータ作成",
+    "ルーティングの設定",
+    "次の記事はありません"
+  ]
+
   def show
     if params[:yyyymm].present? && params[:yyyymmdd].present?
-      @page_title1 = "Ruby on Railsでタスク管理を作ろう！"
-      @page_title2 = ""
-      if params[:yyyymmdd] == "20170926"
-        @page_title2 = "事前準備"
-      elsif params[:yyyymmdd] == "20170927"
-        @page_title2 = "作成するアプリの仕様"
-      elsif params[:yyyymmdd] == "20170930"
-        @page_title2 = "railsプロジェクト作成"
-      elsif params[:yyyymmdd] == "20171004"
-        @page_title2 = "データベース作成"
-      elsif params[:yyyymmdd] == "20171005"
-        @page_title2 = "テストデータ作成"
-      end
+      index = TK_YMD.index(params[:yyyymmdd])
+      @page_title1 = TK_TITLE1
+      @page_title2 = TK_TITLE2[index]
       @page_title = @page_title1 + " " + @page_title2
-      render "articles/" + params[:yyyymm] + "/" + params[:yyyymmdd]
+      @next_page_url = "/articles/" + TK_YMD[index+1][0..5] + "/" + TK_YMD[index+1]
+      @next_page_title = TK_TITLE2[index+1]
+
+      render "articles/" + TK_YMD[index][0..5] + "/" + TK_YMD[index]
     else
       redirect_to root_path
     end
